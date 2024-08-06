816118831ac598e389b4e0ce43558a94a8f71d62
Camilla Guerra Rincon <danna_cguerra@soy.sena.edu.co>
2024-08-06 17:52:29 -0500

    lab02

---
+++ BibliotecaWebApplication/Models/Autor.cs
@@ -0,0 +1,13 @@
+﻿namespace BibliotecaWebApplication.Models
+{
+    public class Autor
+    {
+        public Guid AutorId { get; set; }
+        public string Apellidos { get; set; }
+        public string Nombre { get; set; }
+        public string Nacionalidad { get; set; }
+
+
+        public Autor 
+    }
+}
